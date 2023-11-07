dc7629420a35dde5e2b752241da1b11784cdb630
inigoohernaezz <114742927+inigoohernaezz@users.noreply.github.com>
2023-11-07 13:00:03 +0100

    Add files via upload

--- IW_BI_08/EquiposBaloncesto/urls.py
+++ IW_BI_08/EquiposBaloncesto/urls.py
@@ -3,6 +3,7 @@ from django.urls import *
 urlpatterns=[
     path('', views.inicio, name='inicio'),
     path('<str:nombre_Categoria>/',views.vistacategoria,name='categoria'),
+    path('<str:nombre_Categoria>/<str:nombre_Equipo>/Informacion', views.vistaEquipoInfo, name='Equipoinfo'),
     #path('<str:nombreCategoria>/<str:nombreEquipo>',views, name='equipo'),*/
     path('<str:nombre_Categoria>/<str:nombre_Equipo>', views.vistaJugadores, name='equipo'),
 ]
--- IW_BI_08/EquiposBaloncesto/views.py
+++ IW_BI_08/EquiposBaloncesto/views.py
@@ -11,7 +11,7 @@ def inicio(Request):
     return render(Request,"prueba.html",context)
 
 def vistacategoria(Request,nombre_Categoria):
-    cat=categoria.objects.get(nombreCategoria=nombre_Categoria)
+    cat=categoria.objects.get( nombreCategoria = nombre_Categoria)
     context={
         'cat':cat,
         }
@@ -23,3 +23,10 @@ def vistaJugadores(Request,nombre_Categoria,nombre_Equipo):
         'equipo': equipo1
     }
     return render(Request,'equipo.html',context)
+
+def vistaEquipoInfo(Request, nombre_Categoria, nombre_Equipo):
+    equipo1 = equipo.objects.get(nombreEquipo = nombre_Equipo)
+    context={
+        'equipo': equipo1,
+    }
+    return render(Request,'equipoInfo.html',context)
