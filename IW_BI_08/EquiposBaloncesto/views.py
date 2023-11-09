cd054f07dc9f122189c78356f7738d070b0d245a
inigoohernaezz <114742927+inigoohernaezz@users.noreply.github.com>
2023-11-09 20:23:11 +0100

    Cambios guapos

--- IW_BI_08/EquiposBaloncesto/views.py
+++ IW_BI_08/EquiposBaloncesto/views.py
@@ -3,12 +3,25 @@ from .models import *
 from django.http import *
 from django.template import *
 # Create your views here.
-def inicio(Request):
-    categorias= categoria.objects.order_by('nombreCategoria')
-    context = { 
-        'categorias': categorias,
-        }
-    return render(Request,"prueba.html",context)
+from django.shortcuts import render
+from .models import categoria, equipo
+
+def inicio(request):
+    categorias = categoria.objects.order_by('nombreCategoria')
+    categorias_con_equipos = []  # Lista para almacenar las categorías con sus respectivos equipos
+
+    for categoria_obj in categorias:
+        equipos_por_categoria = equipo.objects.filter(categoria=categoria_obj)
+        categorias_con_equipos.append({
+            'categoria': categoria_obj,
+            'equipos': equipos_por_categoria
+        })
+
+    context = {
+        'categorias_con_equipos': categorias_con_equipos
+    }
+    return render(request, "inicio.html", context)
+
 
 def vistacategoria(Request,nombre_Categoria):
     cat=categoria.objects.get( nombreCategoria = nombre_Categoria)
