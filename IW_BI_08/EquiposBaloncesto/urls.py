d65b7391d6dadebee4752b0c557643c334140858
pablodco <100701377+pablodco@users.noreply.github.com>
2023-11-10 20:04:15 +0100

    depuracion

--- IW_BI_08/EquiposBaloncesto/urls.py
+++ IW_BI_08/EquiposBaloncesto/urls.py
@@ -1,10 +1,14 @@
 from . import views
 from django.urls import *
 urlpatterns=[
-    path('', views.inicio, name='inicio'),
+    path('', views.listaCategorias.as_view(), name='inicio'), 
+    path('<str:nombre_Categoria>/<slug:pk>', views.EquipoInfo.as_view(), name='equipo'),
+    #path('<str:nombre_Categoria>/<str:nombreEquipo>/<slug:pk>', views.jugadorInfo.as_view(), name='jugador'),
+    #path('<slug:pk>', views.CategoriaInfo.as_view(), name='categoria'),
     path('<str:nombre_Categoria>/',views.vistacategoria,name='categoria'),
     path('<str:nombre_Categoria>/<str:nombre_Equipo>/Informacion', views.vistaEquipoInfo, name='Equipoinfo'),
     #path('<str:nombreCategoria>/<str:nombreEquipo>',views, name='equipo'),*/
     path('<str:nombre_Categoria>/<str:nombre_Equipo>', views.vistaJugadores, name='equipo'),
     path('noticias', views.vistaNoticias, name='noticia'),
+    path('contacto', views.contacto ,name='contacto')
 ]
--- IW_BI_08/EquiposBaloncesto/views.py
+++ IW_BI_08/EquiposBaloncesto/views.py
@@ -4,6 +4,7 @@ from django.http import *
 from django.template import *
 # Create your views here.
 from django.shortcuts import render
+from django.views.generic import DetailView,ListView
 from .models import categoria, equipo
 
 def inicio(request):
@@ -50,3 +51,65 @@ def vistaNoticias(Request):
         'categorias': categorias,
         }
     return render(Request,"noticia.html",context)
+
+def inicio(Request):
+    categorias= categoria.objects.order_by('nombreCategoria')
+    context = { 
+        'categorias': categorias,
+        }
+    return render(Request,"prueba.html",context)
+
+def vistacategoria(Request,nombre_Categoria):
+    cat=categoria.objects.get( nombreCategoria = nombre_Categoria)
+    context={
+        'cat':cat,
+        }
+    return render(Request,"categoria.html",context)
+
+def vistaJugadores(Request,nombre_Categoria,nombre_Equipo):
+    equipo1=equipo.objects.get(nombreEquipo=nombre_Equipo)
+    context = {
+        'equipo': equipo1
+    }
+    return render(Request,'equipo.html',context)
+
+def vistaEquipoInfo(Request, nombre_Categoria, nombre_Equipo):
+    equipo1 = equipo.objects.get(nombreEquipo = nombre_Equipo)
+    context={
+        'equipo': equipo1,
+    }
+    return render(Request,'equipoInfo.html',context)
+
+def contacto(Request):
+    return render(Request,'contacto.html')
+class listaEquipos(ListView):
+    model= equipo
+    template_name="listaEquipos.html"
+    context_object_name= 'equipo'
+
+class listaJugadores(ListView):
+    model= jugador
+    template_name='jugadores.html'
+    context_object_name= 'jugador'
+
+
+class jugadorInfo(DetailView):
+    model= jugador
+    template_name='infoJugador.html'
+    context_object_name= 'jugador'
+
+class EquipoInfo(DetailView):
+    model= equipo
+    template_name='equipo.html'
+    context_object_name= 'equipo'
+
+
+class CategoriaInfo(DetailView):
+    model= categoria
+    template_name='categoria.html'
+    context_object_name= 'cat'
+
+class listaCategorias(ListView):
+    model= categoria
+    template_name='inicio.html'
+    context_object_name= 'categorias'
